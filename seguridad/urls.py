a36fea54b174e8ffb4b81a306015ef58d657ca91
Elias Arevalo <ychooop@gmail.com>
2024-07-17 16:57:22 -0400

    Add refresh method.

--- seguridad/urls.py
+++ seguridad/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CustomTokenObtainPairView
+from .views import CustomTokenObtainPairView, CustomTokenRefreshView
 
 
 urlpatterns = [
     path('seguridad/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('seguridad/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
 ]
--- seguridad/views.py
+++ seguridad/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.permissions import AllowAny
 
 # Create your views here.
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
+    permission_classes = [AllowAny]
+
+    # get return value of post method and add username to it
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        # Add the user's username to the response data, bring the username from the login request
+        response.data['name'] = request.data['username']
+        return response
+
+
+class CustomTokenRefreshView(TokenRefreshView):
     permission_classes = [AllowAny]
